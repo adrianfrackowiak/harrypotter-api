893c26eb74dad3ef54ea8cdd69e4693c8cf406c4
adrianfrackowiak <adrianfrackowiak@icloud.com>
2021-06-29 17:10:36 +0200

    add data to api

--- source/controllers/controller.ts
+++ source/controllers/controller.ts
@@ -1,7 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
-const dataBooks = require('../../data/books.json');
-const dataHouses = require('../../data/houses.json');
+const dataBooks = require('../data/books.json');
+const dataHouses = require('../data/houses.json');
+
+const home = (req: Request, res: Response, next: NextFunction) => {
+    return res.sendfile('public/index.html');
+};
 
 const books = (req: Request, res: Response, next: NextFunction) => {
     return res.status(200).json(dataBooks);
@@ -11,4 +15,4 @@ const houses = (req: Request, res: Response, next: NextFunction) => {
     return res.status(200).json(dataHouses);
 };
 
-export default { books, houses };
+export default { home, books, houses };
--- source/routes/router.ts
+++ source/routes/router.ts
@@ -3,6 +3,7 @@ import controller from '../controllers/controller';
 
 const router = express.Router();
 
+router.get('/', controller.home);
 router.get('/books', controller.books);
 router.get('/houses', controller.houses);
 
